540b97ee01637dfe401c938f4cf672a6a246a0c3
Nur Shomik <nurShom@users.noreply.github.com>
2018-11-15 12:01:28 -0800

    Added comments

--- update-gdrive.sh
+++ update-gdrive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Use to update specific directories using the 'drive' application for Google Drive on Linux
-
+# Use to update specific directories outside Drive directory using the 'drive' application for Google Drive on Linux
+# Source of drive app: https://github.com/odeke-em/drive
 
 cd ~/gdrive
 echo "Pulling files"
 drive pull -ignore-name-clashes
 echo "Mounting folders from Documents"
-sudo mount --bind /path/to/target/ newDirMountedInGdrive
+sudo mount --bind ~/Documents/path/to/target newDirMountedInGDrive
 echo "Mounting complete"
 drive push -ignore-name-clashes
 echo "Google Drive push done."
